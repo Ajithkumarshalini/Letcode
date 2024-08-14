7001a368d56681f4d25e19ad4e9b6cb9ac58ace3
Ajithkumarshalini <sjuaajithkumar23@gmail.com>
2024-08-14 18:21:46 +0530

    alert and screenshot

--- src/main/java/homeWork/BrowserLaunch12.java
+++ src/main/java/homeWork/BrowserLaunch12.java
@@ -1,7 +1,5 @@
 package homeWork;
 
-import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver.Navigation;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class BrowserLaunch12 {
