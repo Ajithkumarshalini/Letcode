efa65e1e303225b11c122e90e9aa153331cfa109
Ajithkumarshalini <sjuaajithkumar23@gmail.com>
2024-08-30 19:56:38 +0530

    08/30/2024

--- src/main/java/homeWork/BrowserLaunch12.java
+++ src/main/java/homeWork/BrowserLaunch12.java
@@ -2,8 +2,12 @@ package homeWork;
 
 import org.openqa.selenium.chrome.ChromeDriver;
 
+
 public class BrowserLaunch12 {
 
+	static String email ="sjuaajithkumar@gmail.com";
+	static String pass = "Ajith@01";
+	
 	public static void main(String[] args) {
 
 		System.setProperty("webdriver.chrome.driver", "./drivers/chromedriver.exe");
@@ -11,8 +15,12 @@ public class BrowserLaunch12 {
 		ChromeDriver driver = new ChromeDriver();
 		driver.manage().window().maximize();
 
-		driver.get("https://letcode.in/buttons");		
-	
+		driver.get("https://letcode.in/signin");		
+		driver.findElementByName("email").sendKeys(email);
+		driver.findElementByName("password").sendKeys(pass);
+		driver.findElementByXPath("//button[.='LOGIN']").click();
+		driver.quit();
+		
 
 	}
 }
